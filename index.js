4f5f0ce1a591e6b93395acb8adc5a3f62f158132
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-01-10 00:56:44 +0500

    hbs

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 import express from "express";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import {engine} from 'express-handlebars'
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { create } from 'express-handlebars';
 
 const app = express();
 
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
-app.set('views', './views')
+const hbs = create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+});
 
+app.engine('hbs', hbs.engine); // Pass hbs.engine directly, not hbs.engine()
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
 app.get("/", (req, res) => {
-  // res.status(200)
-  // res.sendFile(path.join(__dirname, "views", "index.html"));
-  res.render('index')
+  res.render('index');
 });
 
 app.get("/about", (req, res) => {
-  // res.status(200)
-  // res.sendFile(path.join(__dirname, "views", "about.html"));
-  res.render('about')
+  res.render('about');
 });
 
 const PORT = process.env.PORT || 4100;
-app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
